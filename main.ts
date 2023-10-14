bf824fc2f533cd641affe21ead1ebc543dd6686d
Jason Rosenbaum <seminal@theseminal.com>
2023-10-14 09:05:32 -0700

    commit

--- main.ts
+++ main.ts
@@ -2,84 +2,71 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface ScrollToBottomOnOpenSettings {
+	notesToScrollToBottom: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ScrollToBottomOnSettings = {
+	//mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ScrollToBottomOnOpen extends Plugin {
+	settings: ScrollToBottomOnSettings;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		
+		this.registerEvent(
+			this.app.workspace.on('file-open', (file) => this.scrollToBottomOfNote(file))
+		);
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.addSettingTab(new ScrollToBottomOnOpenSettingTab(this.app, this));
+		
+		// This adds an editor command that can perform some operation on the current editor instance
+		this.addCommand({
+			id: 'scroll-to-bottom',
+			name: 'Scroll to bottom of current note',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				//console.log(editor.getSelection());
+				this.scrollToBottomOfNote();
+			}
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
+	}
+	
+	//todo: test on mobile
+	
+	async scrollToBottomOfNote(file) {
+		//console.log('loaded');
+		//new Notice('loaded');
+		//console.log(file);
+		//console.log(this.settings.notesToScrollToBottom);
+		let activeFilesArray = this.settings.notesToScrollToBottom.split(",").map(s => s.trim());
+		if ((file && activeFilesArray.includes(file.name)) || !file) {
+			let editor = this.getEditor();
+			if (editor) {
+				let doc = editor.getDoc();
+				//console.log(doc);
+				let cursorHead = editor.getCursor("head");
+				//console.log(cursorHead);
+				let lineLength = doc.getLine(cursorHead.line).length;
+				//console.log(lineLength);
+				let scrollInfo = editor.getScrollInfo();
+				//console.log(scrollInfo);
+				let lines = editor.lineCount();
+				//console.log(lines);
+				editor.setCursor(lines);
+			}
+		}
+		
+	}
+	
+	private getEditor(): Editor {
+		return this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
 	}
 
 	async loadSettings() {
@@ -91,26 +78,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ScrollToBottomOnOpenSettingTab extends PluginSettingTab {
+	plugin: ScrollToBottomOnOpen;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ScrollToBottomOnOpen) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +92,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('File Names To Scroll To Bottom On Open')
+			.setDesc('Enter the names of the files you want to scroll to the bottom of on open, comma separated.')
+			.addTextArea(text => text
+				.setPlaceholder('Enter file names, comma separated')
+				.setValue(this.plugin.settings.notesToScrollToBottom)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.notesToScrollToBottom = value;
 					await this.plugin.saveSettings();
 				}));
 	}
